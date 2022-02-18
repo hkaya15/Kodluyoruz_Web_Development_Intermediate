ed8b5569a0015a03011f631649d94d8472978a8b
hkaya15 <kayahuseyin15@gmail.com>
2022-02-19 01:14:12 +0300

    created todos app

---
+++ React/Ödev-2/todo/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <section className="todoapp">
+      <header className="header">
+        <h1>todos</h1>
+      </header>
+    </section>
+  );
+}
+
+export default App;
