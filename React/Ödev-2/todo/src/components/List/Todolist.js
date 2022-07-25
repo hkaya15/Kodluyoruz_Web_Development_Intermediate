1737e1e7921564aa5e0cc84ad0d8f5098fd3e9ff
hkaya15 <kayahuseyin15@gmail.com>
2022-07-26 00:38:45 +0300

    Add todo-item delete feature

--- React/Ödev-2/todo/src/components/List/Todolist.js
+++ React/Ödev-2/todo/src/components/List/Todolist.js
@@ -1,25 +1,36 @@
 
-function TodoList({wholeList, setList}){
-    
-    return <section className="main">
-          <input className="toggle-all" type="checkbox"/>
-          <label htmlFor="toggle-all">Mark all as complete</label>
+function TodoList({ wholeList, setList }) {
 
-          <ul className="todo-list">
-            {wholeList.map((key,i)=>{
-              return <li key={i} className={key.class === false ? "" : "completed"}>
-                <div className="view">
-                  <input className="toggle" type="checkbox" onChange={()=>{
-                    wholeList[i].class=!wholeList[i].class;
-                    setList([...wholeList])
-                  }}/>
-                  <label>{key.value}</label>
-                  <button className="destroy"></button>
-                </div>
-              </li>
-            })}
-          </ul>
-      </section>
+  const deleteItem = (key) => {
+
+    wholeList.map((e) => {
+      if (e === key) {
+        const index = wholeList.indexOf(e);
+        wholeList.splice(index, 1)
+        setList([...wholeList])
+      }
+    })
+  }
+
+  return <section className="main">
+    <input className="toggle-all" type="checkbox" />
+    <label htmlFor="toggle-all">Mark all as complete</label>
+
+    <ul className="todo-list">
+      {wholeList.map((key, i) => {
+        return <li key={i} className={key.class === false ? "" : "completed"}>
+          <div className="view">
+            <input className="toggle" type="checkbox" onChange={() => {
+              wholeList[i].class = !wholeList[i].class;
+              setList([...wholeList])
+            }} />
+            <label>{key.value}</label>
+            <button className="destroy" onClick={(i) => deleteItem(key)}></button>
+          </div>
+        </li>
+      })}
+    </ul>
+  </section>
 }
 
 
